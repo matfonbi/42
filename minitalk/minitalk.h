379f7b4a1d1bb4313925f7a08c85f065bdd27252
matfonbi <mathis.fonbonne@gmail.com>
2025-05-13 08:51:08 +0200

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajordan- <ajordan-@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 16:12:10 by ajordan-          #+#    #+#             */
+/*   Updated: 2022/02/20 13:11:22 by ajordan-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../libft/inc/libft.h"
+# include "../libft/inc/ft_printf.h"
+# include <signal.h>
+
+#endif
